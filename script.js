ab6c86be2cd2657a9aa8a4b25ce80cbcfae0f630
GuinhoFSilva <guilherme.silva@sptech.school>
2024-12-17 13:13:16 -0300

    alterando as fotos

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function mudarTema(){
     const img = document.querySelector("#profile img")
 
     if(document.body.classList.contains('light')){
-        img.setAttribute('src', './assets/perfil-light.PNG')
+        img.setAttribute('src', './assets/perfil-light.jpeg')
         img.setAttribute('alt', 'foto de perfil tema claro')
     }else{
-        img.setAttribute('src', './assets/perfil-dark.jpg')
+        img.setAttribute('src', './assets/perfil-dark.jpeg')
         img.setAttribute('alt', 'foto de perfil tema escuro')
     }
 }
