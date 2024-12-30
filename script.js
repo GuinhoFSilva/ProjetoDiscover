19bc4255af114d58bb89f0eb06b3b2245ba56bee
GuinhoFSilva <guilherme.silva@sptech.school>
2024-12-30 10:23:29 -0300

    Adicionando conexão com Linkedin

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ function mudarTema(){
     body.classList.toggle('light')
 
     const img = document.querySelector("#profile img")
+    const link = document.getElementById('favicon')
 
     if(document.body.classList.contains('light')){
         img.setAttribute('src', './assets/perfil-light.jpeg')
         img.setAttribute('alt', 'foto de perfil tema claro')
+        link.setAttribute('href', './assets/favicon.png')
     }else{
         img.setAttribute('src', './assets/perfil-dark.jpeg')
         img.setAttribute('alt', 'foto de perfil tema escuro')
+        link.setAttribute('href', './assets/4187236.png')
     }
 }
